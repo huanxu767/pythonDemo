0c5bc5d89008aa32e70c999923ca6cd14ca7373c
许先生 <huanxu767@qq.com>
2017-11-29 17:33:12 +0800

    归属地

--- download.py
+++ download.py
@@ -1,4 +1,6 @@
 import requests
+
+# https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?resource_name=guishudi&query=1391381+%E6%89%8B%E6%9C%BA%E5%8F%B7%E6%AE%B5
 res = requests.get("http://www.baidu.com")
 savefile = open("baidu.html","w")
 savefile.write(res.content)
