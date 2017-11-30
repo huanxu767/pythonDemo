94df0c86f8a2b925aaba506bb4274cc79132866d
xuhuan <huanxu767@qq.com>
2017-11-30 22:50:54 +0800

    类

---
+++ classDemo.py
@@ -0,0 +1,14 @@
+class Employee:
+    classSpec = "this a test class"
+    def __init__(self,name,salary):
+        self.name = name
+        self.salary = salary
+
+    def hello(self):
+        # print 'asdasdads'
+        print "hello,"+self.name + ",your salary is " + str(self.salary)
+
+
+worker = Employee("xiaoxu",100000)
+worker.hello()
+print worker.salary
---
+++ diaoyong.py
@@ -0,0 +1,3 @@
+from classDemo import Employee
+worker = Employee("xiaoxu",100000)
+worker.hello()
